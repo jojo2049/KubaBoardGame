a1cafd77f3b81a999db85875d891e7f5dbb1456b
jojo2049 <jojo2049@gmail.com>
2021-05-31 22:55:44 -0500

    Started class/method layout.

---
+++ KubaGame.py
@@ -0,0 +1,44 @@
+# Author: Jonathan Luong
+# Date: 5/31/21
+# Description: A program clone of Kuba, the board game
+
+
+class KubaGame:
+    """
+    This class is our overall game class.
+    Contains all methods for initializing the game and various moves/options a player has.
+
+    """
+    def __init__(self, p1, p2):
+        self.player_1
+        pass
+
+    def get_current_turn(self):
+        pass
+
+    def make_move(self, playername, coordinates, direction):
+        pass
+
+    def get_winner(self):
+        pass
+
+    def get_captured(self):
+        pass
+
+    def get_marble(self):
+        pass
+
+    def get_marble_count(self):
+        pass
+
+
+class GameBoard:
+
+    def __init__(self):
+        pass
+
+
+class Marble:
+
+    def __init__(self):
+        pass
