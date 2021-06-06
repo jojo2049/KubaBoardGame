0afb41570aed5be75810cf1c15638596b53a942b
jojo2049 <jojo2049@gmail.com>
2021-06-06 00:10:52 -0500

    Converting board to marble objects.

--- KubaGame.py
+++ KubaGame.py
@@ -9,17 +9,20 @@ class KubaGame:
     Contains all methods for initializing the game and various moves/options a player has.
     Takes as its parameters two tuples and will work the GameBoard, Player, and Marble objects.
     """
+
     def __init__(self, p1: tuple, p2: tuple):
         """
         initializes the Player, GameBoard, and Marble objects of the game
         :param p1: player name and color of the marble ('PlayerA', 'B')
         :param p2: player name and color of the marble ('PlayerB', 'W')
         """
-        self.p1_name = p1[0]
-        self.p2_name = p2[0]
-        self.p1_color = p1[1]
-        self.p2_color = p2[1]
-
+        # self.p1_name = p1[0]
+        # self.p2_name = p2[0]
+        # self.p1_color = p1[1]
+        # self.p2_color = p2[1]
+        self.p1 = Player(p1[0], p1[1])
+        self.p2 = Player(p1[0], p1[1])
+        self.gameboard = Board()
 
     def get_current_turn(self):
         """
@@ -74,11 +77,13 @@ class Board:
     Class for the game board object. Will contain code for visualizing the board and any methods for changing/updating this.
 
     """
+
     def __init__(self):
         """
         Initializes properties for the game board.
         """
         self.board = {}
+        w1,w2,w3,w4,w5,w6,w7,w8 = White()
 
     def create_board(self):
         squares = []
@@ -91,7 +96,7 @@ class Board:
             column += 1
             row = 1
 
-        for (x,y) in squares:
+        for (x, y) in squares:
             if x < 3 and y < 3:
                 self.board.update({(x, y): 'W'})
             elif x > 5 and y > 5:
@@ -109,28 +114,40 @@ class Board:
             else:
                 self.board.update({(x, y): 'X'})
 
-
     def get_board(self):
         """
         Methods to get a visual of the board
         :return: a visual print of the board in console
         """
         print('--+-+-+-+-+-+--')
-        print('|'+self.board.get((1,1))+'|'+self.board.get((2,1))+'|'+self.board.get((3,1))+'|'+self.board.get((4,1))+'|'+self.board.get((5,1))+'|'+self.board.get((6,1))+'|'+self.board.get((7,1))+'|')
+        print('|' + self.board.get((1, 1)) + '|' + self.board.get((2, 1)) + '|' + self.board.get(
+            (3, 1)) + '|' + self.board.get((4, 1)) + '|' + self.board.get((5, 1)) + '|' + self.board.get(
+            (6, 1)) + '|' + self.board.get((7, 1)) + '|')
         print('--+-+-+-+-+-+--')
-        print('|'+self.board.get((1,2))+'|'+self.board.get((2,2))+'|'+self.board.get((3,2))+'|'+self.board.get((4,2))+'|'+self.board.get((5,2))+'|'+self.board.get((6,2))+'|'+self.board.get((7,2))+'|')
+        print('|' + self.board.get((1, 2)) + '|' + self.board.get((2, 2)) + '|' + self.board.get(
+            (3, 2)) + '|' + self.board.get((4, 2)) + '|' + self.board.get((5, 2)) + '|' + self.board.get(
+            (6, 2)) + '|' + self.board.get((7, 2)) + '|')
         print('--+-+-+-+-+-+--')
-        print('|'+self.board.get((1,3))+'|'+self.board.get((2,3))+'|'+self.board.get((3,3))+'|'+self.board.get((4,3))+'|'+self.board.get((5,3))+'|'+self.board.get((6,3))+'|'+self.board.get((7,3))+'|')
+        print('|' + self.board.get((1, 3)) + '|' + self.board.get((2, 3)) + '|' + self.board.get(
+            (3, 3)) + '|' + self.board.get((4, 3)) + '|' + self.board.get((5, 3)) + '|' + self.board.get(
+            (6, 3)) + '|' + self.board.get((7, 3)) + '|')
         print('--+-+-+-+-+-+--')
-        print('|'+self.board.get((1,4))+'|'+self.board.get((2,4))+'|'+self.board.get((3,4))+'|'+self.board.get((4,4))+'|'+self.board.get((5,4))+'|'+self.board.get((6,4))+'|'+self.board.get((7,4))+'|')
+        print('|' + self.board.get((1, 4)) + '|' + self.board.get((2, 4)) + '|' + self.board.get(
+            (3, 4)) + '|' + self.board.get((4, 4)) + '|' + self.board.get((5, 4)) + '|' + self.board.get(
+            (6, 4)) + '|' + self.board.get((7, 4)) + '|')
         print('--+-+-+-+-+-+--')
-        print('|'+self.board.get((1,5))+'|'+self.board.get((2,5))+'|'+self.board.get((3,5))+'|'+self.board.get((4,5))+'|'+self.board.get((5,5))+'|'+self.board.get((6,5))+'|'+self.board.get((7,5))+'|')
+        print('|' + self.board.get((1, 5)) + '|' + self.board.get((2, 5)) + '|' + self.board.get(
+            (3, 5)) + '|' + self.board.get((4, 5)) + '|' + self.board.get((5, 5)) + '|' + self.board.get(
+            (6, 5)) + '|' + self.board.get((7, 5)) + '|')
         print('--+-+-+-+-+-+--')
-        print('|'+self.board.get((1,6))+'|'+self.board.get((2,6))+'|'+self.board.get((3,6))+'|'+self.board.get((4,6))+'|'+self.board.get((5,6))+'|'+self.board.get((6,6))+'|'+self.board.get((7,6))+'|')
+        print('|' + self.board.get((1, 6)) + '|' + self.board.get((2, 6)) + '|' + self.board.get(
+            (3, 6)) + '|' + self.board.get((4, 6)) + '|' + self.board.get((5, 6)) + '|' + self.board.get(
+            (6, 6)) + '|' + self.board.get((7, 6)) + '|')
         print('--+-+-+-+-+-+--')
-        print('|'+self.board.get((1,7))+'|'+self.board.get((2,7))+'|'+self.board.get((3,7))+'|'+self.board.get((4,7))+'|'+self.board.get((5,7))+'|'+self.board.get((6,7))+'|'+self.board.get((7,7))+'|')
+        print('|' + self.board.get((1, 7)) + '|' + self.board.get((2, 7)) + '|' + self.board.get(
+            (3, 7)) + '|' + self.board.get((4, 7)) + '|' + self.board.get((5, 7)) + '|' + self.board.get(
+            (6, 7)) + '|' + self.board.get((7, 7)) + '|')
         print('--+-+-+-+-+-+--')
-
 
 
 class Player:
@@ -138,12 +155,19 @@ class Player:
     Class for player object and various methods related to them.
     Will record the player information and keep track of whose turn it is
     """
+
     def __init__(self, name, color):
         """
         Will initialize the properties of a player
         :param name: Player Name
         :param color: Color of Marbles
         """
+        self.name = name
+        self.color = color
+        self.marbles
+
+    def player_marbles(self):
+        # if self.color =
         pass
 
     def get_turn_player(self):
@@ -176,13 +200,15 @@ class Player:
         """
         pass
 
+
 class Marble:
     """
     Class for marble object of the board game.
     Controls whether or not a marble can move in a given direction
     """
+
     def __init__(self):
-        pass
+        self.color = None
 
     def backward(self):
         """
@@ -212,6 +238,23 @@ class Marble:
         """
         pass
 
+class White(Marble):
+
+    def __init__(self):
+        self.color = 'W'
+
+
+class Black(Marble):
+
+    def __init__(self):
+        self.color = 'B'
+
+
+class Red(Marble):
+
+    def __init__(self):
+        self.color = 'R'
+
 class Queue:
     """
     An implementation of the Queue ADT that uses Python's built-in lists
@@ -232,5 +275,7 @@ class Queue:
     def is_empty(self):
         return len(self.list) == 0
 
-kubaboard = KubaGame(('PlayerA', 'W'), ('PlayerB', 'B'))
 
+game = KubaGame(('PlayerA', 'W'), ('PlayerB', 'B'))
+game.gameboard.create_board()
+game.gameboard.get_board()
